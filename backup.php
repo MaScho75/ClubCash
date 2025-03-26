3dac6ed65f58128e544fb872b239d536b85d35c5
Marcel Schommer <marcel@schommer.berlin>
2025-03-27 00:14:36 +0100

    2. Sicherheitsbackup installiert. Funktion noch nicht überprüft.

--- backup.php
+++ backup.php
@@ -3,10 +3,12 @@
 // Pfade definieren
 define('SOURCE_FILE', 'daten/verkaufsliste.csv');
 define('BACKUP_DIR', 'backup/');
+define('BACKUP_DIR2', 'https://schommer.berlin/ClubCash_DatenBackup/');
 
 // Datum für den Dateinamen erzeugen
 $date = date('y-m-d');
 $backupFile = BACKUP_DIR . "verkaufsliste - $date.csv";
+$backupFile2 = BACKUP_DIR2 . "verkaufsliste - $date.csv";
 
 // Prüfen, ob die Sicherung für heute bereits existiert
 if (!file_exists($backupFile)) {
@@ -23,6 +25,12 @@ if (!file_exists($backupFile)) {
     }
 } else {
     echo "Backup für heute existiert bereits.";
+}
+
+if (copy(SOURCE_FILE, $backupFile2)) {
+    echo "Backup2 erfolgreich erstellt: $backupFile";
+} else {
+    echo "Fehler beim Erstellen des Backup2.";
 }
 
 ?>
