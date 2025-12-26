52229753c7c7716bd2606a5b413f197b3768161b
Marcel Schommer <marcel@schommer.berlin>
2025-12-26 19:48:43 +0100

    offlinefunktionalität vereinfacht

--- kasse/sw.js
+++ kasse/sw.js
@@ -15,7 +15,7 @@
  * along with ClubCash. If not, see <https://www.gnu.org/licenses/>.
  */
 
-const CACHE_NAME = 'clubcash-v2';
+const CACHE_NAME = 'clubcash-v3';
 const ASSETS = [
   './',
   './index.html',
